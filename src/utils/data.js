c8cdf1ccff834ded17ee1462083f4ea744261a1e
eFootballFans <138303609+eFootballFans@users.noreply.github.com>
2025-01-01 21:11:15 +0000

    Update legacy winners data

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,201 +1,422 @@
 export const hallOfFameJsonData = {
-    "hall_of_fame": [
-      // Your JSON data here
-      {
-        "id": 5,
-        "tournament_name": "eFootball Mobile Fun Friendly Cup Dec'24",
-        "game_name": "eFootball 2025 Mobile",
-        "platform": "Telegram",
-        "tournament_link": "https://t.me/eFootballFansTournament/146",
-        "instagram_link": "https://instagram.com/eFootballFansOfficial",
-        "year": 2024,
-        "start_date": "2024-12-09",
-        "end_date": "2024-12-24",
-        "participant_count": "32",
-        "winners": [
-          {
-            "rank": 1,
-            "first_name": "Tax",
-            "last_name": "Evader",
-            "country": "USA",
-            "place": null,
-            "telegram_id": "112233445",
-            "discord_id": null,
-            // "telegram_username": "@E_T_H_A_NN",
-            "telegram_username": null,
-            "discord_username": null,
-            "instagram_username": "ethan_shivere_"
-          },
-          {
-            "rank": 2,
-            "first_name": "Rohit",
-            "last_name": "Sawant",
-            "country": "India",
-            "place": "Mapusa, Goa, India",
-            "telegram_id": "223344556",
-            "discord_id": null,
-            "telegram_username": null,
-            "discord_username": null,
-            "instagram_username": "rohitsawant19"
-          },
-          {
-            "rank": 3,
-            "first_name": "Champagne",
-            "last_name": "Pappi",
-            "country": "USA",
-            "place": null,
-            "telegram_id": "334455667",
-            "discord_id": null,
-            "telegram_username": null,
-            "discord_username": null,
-            "instagram_username": "Champagne_pappii_e"
-          }
-        ]
-      },
-      {
-        "id": 4,
-        "tournament_name": "eFootball European Championship",
-        "game_name": "eFootball 2023",
-        "platform": "Discord",
-        "tournament_link": "https://discord.gg/eFootballEuropeanChampionship",
-        "instagram_link": null,
-        "year": 2023,
-        "start_date": "2023-07-01",
-        "end_date": "2023-07-05",
-        "participant_count": "16",
-        "winners": [
-          {
-            "rank": 1,
-            "first_name": "Jane",
-            "last_name": "Smith",
-            "telegram_id": null,
-            "discord_id": "jane_smith#4321",
-            "telegram_username": null,
-            "discord_username": "jane_smith#4321"
-          },
-          {
-            "rank": 2,
-            "first_name": "Emily",
-            "last_name": "Jones",
-            "telegram_id": null,
-            "discord_id": "emily_jones#5678",
-            "telegram_username": null,
-            "discord_username": "emily_jones#5678"
-          }
-        ]
-      },
-      {
-        "id": 3,
-        "tournament_name": "eFootball Global League",
-        "game_name": "eFootball 2022",
-        "platform": "Telegram",
-        "tournament_link": "https://t.me/eFootballGlobalLeague",
-        "instagram_link": null,
-        "year": 2022,
-        "start_date": "2022-09-10",
-        "end_date": "2022-09-15",
-        "participant_count": "32",
-        "winners": [
-          {
-            "rank": 1,
-            "first_name": "Michael",
-            "last_name": "Williams",
-            "telegram_id": "112233445",
-            "discord_id": null,
-            "telegram_username": "@michael_williams_9988",
-            "discord_username": null
-          },
-          {
-            "rank": 2,
-            "first_name": "David",
-            "last_name": "Taylor",
-            "telegram_id": "223344556",
-            "discord_id": null,
-            "telegram_username": "@david_taylor_2233",
-            "discord_username": null
-          },
-          {
-            "rank": 3,
-            "first_name": "Sarah",
-            "last_name": "Brown",
-            "telegram_id": "334455667",
-            "discord_id": null,
-            "telegram_username": "@sarah_brown_1122",
-            "discord_username": null
-          }
-        ]
-      },
-      {
-        "id": 2,
-        "tournament_name": "eFootball Pro League",
-        "game_name": "eFootball 2024",
-        "platform": "Discord",
-        "tournament_link": "https://discord.gg/eFootballProLeague",
-        "instagram_link": "https://instagram.com/eFootballProLeague",
-        "year": 2024,
-        "start_date": "2024-03-05",
-        "end_date": "2024-03-10",
-        "winners": [
-          {
-            "rank": 1,
-            "first_name": "Alex",
-            "last_name": "Green",
-            "telegram_id": null,
-            "discord_id": "alex_green#5566",
-            "telegram_username": null,
-            "discord_username": "alex_green#5566"
-          },
-          {
-            "rank": 2,
-            "first_name": "Olivia",
-            "last_name": "White",
-            "telegram_id": null,
-            "discord_id": "olivia_white#2233",
-            "telegram_username": null,
-            "discord_username": "olivia_white#2233"
-          },
-          {
-            "rank": 3,
-            "first_name": "Chris",
-            "last_name": "Johnson",
-            "telegram_id": null,
-            "discord_id": "chris_johnson#4455",
-            "telegram_username": null,
-            "discord_username": "chris_johnson#4455"
-          },
-          {
-            "rank": 4,
-            "first_name": "Sophia",
-            "last_name": "Martinez",
-            "telegram_id": null,
-            "discord_id": "sophia_martinez#6677",
-            "telegram_username": null,
-            "discord_username": "sophia_martinez#6677"
-          }
-        ]
-      },
-      {
-        "id": 1,
-        "tournament_name": "eFootball World Championship",
-        "game_name": "eFootball 2023",
-        "platform": "Telegram",
-        "tournament_link": "https://t.me/eFootballWorldChampionship",
-        "instagram_link": "https://instagram.com/eFootballWorldChampionship",
-        "year": 2023,
-        "start_date": "2021-06-15",
-        "end_date": "2021-06-20",
-        "participant_count": "8",
-        "winners": [
-          {
-            "rank": 1,
-            "first_name": "John",
-            "last_name": "Doe",
-            "telegram_id": "123456789",
-            "discord_id": null,
-            "telegram_username": "@john_doe_123",
-            "discord_username": null
-          }
-        ]
-      }
-    ]
-  };
+  "hall_of_fame": [
+    // Your JSON data here
+    {
+      "id": 40,
+      "tournament_name": "eFootball Mobile Fun Friendly Cup Dec'24",
+      "game_name": "eFootball 2025 Mobile",
+      "platform": "Telegram",
+      "tournament_link": "https://t.me/eFootballFansTournament/146",
+      "instagram_link": "https://instagram.com/eFootballFansOfficial",
+      "year": 2024,
+      "start_date": "2024-12-09",
+      "end_date": "2024-12-24",
+      "participant_count": "32",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "Tax",
+          "last_name": "Evader",
+          "country": "USA",
+          "place": null,
+          "telegram_id": "112233445",
+          "discord_id": null,
+          // "telegram_username": "@E_T_H_A_NN",
+          "telegram_username": null,
+          "discord_username": null,
+          "instagram_username": "ethan_shivere_"
+        },
+        {
+          "rank": 2,
+          "first_name": "Rohit",
+          "last_name": "Sawant",
+          "country": "India",
+          "place": "Mapusa, Goa, India",
+          "telegram_id": "223344556",
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": null,
+          "instagram_username": "rohitsawant19"
+        },
+        {
+          "rank": 3,
+          "first_name": "Champagne",
+          "last_name": "Pappi",
+          "country": "USA",
+          "place": null,
+          "telegram_id": "334455667",
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": null,
+          "instagram_username": "Champagne_pappii_e"
+        }
+      ]
+    },
+
+    {
+      "id": 15,
+      "tournament_name": "PIC Championship League 1 - Final Championship (Sponsored by Mackie PES HD)",
+      "game_name": "PES 2021 Mobile",
+      "platform": "Discord",
+      "tournament_link": "https://discord.com/channels/755155893154480289/766012914712182804/796733707058806795",
+      "instagram_link": null,
+      "year": 2020,
+      "start_date": "2020-12-19",
+      "end_date": "2021-01-07",
+      "participant_count": "64",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "Albzzz_",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "albzzz_"
+        },
+        {
+          "rank": 2,
+          "first_name": "NR77",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "nr77__"
+        },
+        {
+          "rank": 3,
+          "first_name": "barca_fan_forever",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "barca_fan_forever3842"
+        }
+      ]
+    },
+    {
+      "id": 14,
+      "tournament_name": "PIC Championship League 1 - Qualifiers 4",
+      "game_name": "PES 2021 Mobile",
+      "platform": "Discord",
+      "tournament_link": "https://discord.com/channels/755155893154480289/766012914712182804/796733707058806795",
+      "instagram_link": null,
+      "year": 2020,
+      "start_date": "2020-12-06",
+      "end_date": "2020-12-18",
+      "participant_count": "16",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "PESky Maestro",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "peskymaestro"
+        },
+        {
+          "rank": 2,
+          "first_name": "Albzzz_",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "albzzz_"
+        }
+      ]
+    },
+    {
+      "id": 13,
+      "tournament_name": "PIC Championship League 1 - Qualifiers 3",
+      "game_name": "PES 2021 Mobile",
+      "platform": "Discord",
+      "tournament_link": "https://discord.com/channels/755155893154480289/766012914712182804/796733707058806795",
+      "instagram_link": null,
+      "year": 2020,
+      "start_date": "2020-12-06",
+      "end_date": "2020-12-18",
+      "participant_count": "16",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "Marvel",
+          "last_name": "Us",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "marvelus5216"
+        },
+        {
+          "rank": 2,
+          "first_name": "Eno_wizzy#6095",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "Eno_wizzy#6095"
+        }
+      ]
+    },
+    {
+      "id": 12,
+      "tournament_name": "PIC Championship League 1 - Qualifiers 2",
+      "game_name": "PES 2021 Mobile",
+      "platform": "Discord",
+      "tournament_link": "https://discord.com/channels/755155893154480289/766012914712182804/796733707058806795",
+      "instagram_link": null,
+      "year": 2020,
+      "start_date": "2020-12-06",
+      "end_date": "2020-12-18",
+      "participant_count": "16",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "venom_mz7#0432",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "venom_mz7#0432"
+        },
+        {
+          "rank": 2,
+          "first_name": "richie",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "richie078177"
+        }
+      ]
+    },
+    {
+      "id": 11,
+      "tournament_name": "PIC Championship League 1 - Qualifiers 1",
+      "game_name": "PES 2021 Mobile",
+      "platform": "Discord",
+      "tournament_link": "https://discord.com/channels/755155893154480289/766012914712182804/796733707058806795",
+      "instagram_link": null,
+      "year": 2020,
+      "start_date": "2020-12-06",
+      "end_date": "2020-12-18",
+      "participant_count": "16",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "Anush07",
+          "last_name": "",
+          "country": "India",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "anush075795"
+        },
+        {
+          "rank": 2,
+          "first_name": "Abnas",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "abnas4873"
+        }
+      ]
+    },
+    {
+      "id": 9,
+      "tournament_name": "PES MOBILE FRIENDLY CUP by @Shetty29",
+      "game_name": "PES 2021 Mobile",
+      "platform": "Discord",
+      "tournament_link": "https://discord.com/channels/755155893154480289/766012914712182804/776908511744622593",
+      "instagram_link": null,
+      "year": 2020,
+      "start_date": "2020-11-01",
+      "end_date": "2020-11-14",
+      "participant_count": "16",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "Voublecaptain",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "voublecaptain"
+        },
+        {
+          "rank": 2,
+          "first_name": "henry_1",
+          "last_name": "",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "henry_18153"
+        },
+        {
+          "rank": 3,
+          "first_name": "Abhyuday",
+          "last_name": "Pal",
+          "country": "India",
+          "place": "West Bengal, India",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "abhyudaypal"
+        }
+      ]
+    },
+    {
+      "id": 8,
+      "tournament_name": "PES 21 Mobile: Silver Cup- Quick Friendly Tournament by FKPG",
+      "game_name": "PES 2021 Mobile",
+      "platform": "Discord",
+      "tournament_link": "https://t.me/eFootballFansGroup/16139",
+      "instagram_link": "https://www.instagram.com/p/CG7jRZeASSV/",
+      "year": 2020,
+      "start_date": "2020-10-26",
+      "end_date": "2020-10-29",
+      "participant_count": "8",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "Abhyuday",
+          "last_name": "Pal",
+          "country": "India",
+          "place": "West Bengal, India",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "abhyudaypal"
+        },
+        {
+          "rank": 2,
+          "first_name": "Anush07",
+          "last_name": "",
+          "country": "India",
+          "telegram_id": null,
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": "anush075795"
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "tournament_name": "P.M.I.C. Friendly Tournament 3",
+      "game_name": "PES Mobile",
+      "platform": "Telegram",
+      "tournament_link": "https://t.me/eFootballFansGroup/30751",
+      "instagram_link": null,
+      "year": 2020,
+      "start_date": "2020-03-25",
+      "end_date": "2020-04-04",
+      "participant_count": "32",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "uzzal",
+          "last_name": "islam",
+          "telegram_id": "000000000",
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": null
+        },
+        {
+          "rank": 2,
+          "first_name": "Natheem",
+          "last_name": "",
+          "telegram_id": "000000000",
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": null
+        },
+        {
+          "rank": 3,
+          "first_name": "MUHAMMAD",
+          "last_name": "RIFAIE",
+          "telegram_id": "000000000",
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": null
+        },
+        {
+          "rank": 4,
+          "first_name": "Arun",
+          "last_name": "",
+          "telegram_id": "000000000",
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": null
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "tournament_name": "P.M.I.C. Friendly Tournament 2",
+      "game_name": "PES Mobile",
+      "platform": "Telegram",
+      "tournament_link": "https://t.me/eFootballFansGroup/16139",
+      "instagram_link": null,
+      "year": 2020,
+      "start_date": "2020-01-25",
+      "end_date": "2020-02-04",
+      "participant_count": "8",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "shanu",
+          "last_name": "Sha",
+          "telegram_id": "000000000",
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": null
+        },
+        {
+          "rank": 2,
+          "first_name": "Nikhil",
+          "last_name": "",
+          "country": "India",
+          "telegram_id": "000000000",
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": null
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "tournament_name": "P.M.I.C. Friendly Tournament 1",
+      "game_name": "PES Mobile",
+      "platform": "Telegram",
+      "tournament_link": "https://t.me/eFootballFansGroup/2888",
+      "instagram_link": null,
+      "year": 2019,
+      "start_date": "2019-11-15",
+      "end_date": "2019-11-14",
+      "participant_count": "16",
+      "winners": [
+        {
+          "rank": 1,
+          "first_name": "Gowrav",
+          "last_name": "",
+          "country": "India",
+          "telegram_id": "000000000",
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": null
+        },
+        {
+          "rank": 2,
+          "first_name": "Nikhil",
+          "last_name": "",
+          "country": "India",
+          "telegram_id": "000000000",
+          "discord_id": null,
+          "telegram_username": null,
+          "discord_username": null
+        }
+      ]
+    }
+  ]
+};
